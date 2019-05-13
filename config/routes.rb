f226200e86060057bd88413c5eab9a6fb12a66d3
Kayla Cohn <kayla.s.cohn@gmail.com>
2019-05-12 22:38:15 -0400

    Add root route and resources routes for questions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: "questions#index"
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/sign_in' => 'devise/sessions#new'
   get '/sign_up' => 'devise/registrations#new'
+
+  resources :questions
 end
