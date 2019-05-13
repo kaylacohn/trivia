57bf8f271e933c279ac7988d4e85c80ab00611e8
Kayla Cohn <kayla.s.cohn@gmail.com>
2019-05-13 01:21:15 -0400

    Add custom registration devise path for controller and add my questions and my answers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   root to: "questions#index"
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get '/sign_in' => 'devise/sessions#new'
-  get '/sign_up' => 'devise/registrations#new'
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :questions do
     resources :choices
 
     patch '/answer' => 'questions#answer'
   end
+
+  get 'my_questions' => 'questions#me'
+  get 'answered_questions' => 'questions#answered_questions'
 end
