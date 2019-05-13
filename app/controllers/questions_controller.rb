719512cf059aaec3af0d004b573e1168cc05b2ab
Kayla Cohn <kayla.s.cohn@gmail.com>
2019-05-12 23:09:15 -0400

    crud question controller work

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,52 @@
 class QuestionsController < ApplicationController
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @questions = Question.published
+  end
+
+  def show
+    @choices = @question.choices
+  end
+
+  def new
+    @question = current_user.questions.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+
+    if @question.save
+      redirect_to @question, notice: "Question was successfully created"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question, notice: "Question was successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title)
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
 end
