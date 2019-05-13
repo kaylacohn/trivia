f4b7be80ffe408cb0bc7e4a9de2af1a75f807d28
Kayla Cohn <kayla.s.cohn@gmail.com>
2019-05-12 22:13:57 -0400

    Add new routes for sign up and sign in routes and update registraion and session views to include name field

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/sign_in' => 'devise/sessions#new'
+  get '/sign_up' => 'devise/registrations#new'
 end
